8d759e8b8304ea5171be3c1d839fa4f518e5fb10
Otavio96 <otavio-2011afonso@hotmail.com>
2019-09-11 21:32:43 -0300

    Alterando o script

--- leonardo_otavio.py
+++ leonardo_otavio.py
@@ -1,2 +1,15 @@
 # coding: utf-8
+
 import re
+
+telefone = input('Digite seu telefone: ')
+cpf = input('Digite seu CPF: ')
+
+pattern_telefone = '(\([0-9]{2}\)?\s)?([0-9]{4,5}\-?[0-9]{4})'
+resultado = re.match(pattern_telefone, telefone)
+
+
+if resultado != None:
+	print("telefone valido!")
+else:
+	print("telefone invalido!")
