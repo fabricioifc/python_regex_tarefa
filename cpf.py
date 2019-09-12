839904774bcf746aa3298608b39365b00c033f40
Leonardo <leovenancio00@gmail.com>
2019-09-11 21:58:16 -0300

    Adicionando script CPF atualizado

--- cpf.py
+++ cpf.py
@@ -1,11 +1,11 @@
 import re
 
-usuario_cpf = str(input("Informe o numero do cpf: ")
+usuario_cpf = input("Informe o numero do cpf: ")
 
-pattern = '/^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/'
+pattern_int = re.compile(r"([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})")
 
 
-if re.match(usuario_cpf):
+if pattern_int.match(usuario_cpf):
     print(usuario_cpf + "É um CPF Válido")
 else:
     print(usuario_cpf + "É um CPF Inválido")
