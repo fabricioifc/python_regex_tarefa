3c569c3c1fc5026268b0ed7d771d1e3364cb9615
gdarold <gadarold@gmail.com>
2019-09-11 21:41:07 -0300

    Tarefa pronta

--- gabriel_luiz.py
+++ gabriel_luiz.py
@@ -5,24 +5,33 @@ import re
 
 nome = input("Digite seu nome: ")
 
-telefone = input("digite seu telefone ( xx xxxxx-xxxx): ")
+telefone = input("Digite seu telefone ( xx xxxxx-xxxx): ")
 
 email = input("Digite seu email: ")
 
 
 
-
-
+pattern_nome = '[A-Z]'
 
 pattern_tel = '([0-9]{2}\s[0-9]{4,5}\-?[0-9]{4})'
 
 pattern_email = '^\w*(\.\w*)?@\w*\.[a-z]+(\.[a-z]+)?$'
 
 
-#resultado = re.search(pattern,telefone) # busca no texto o padrão
-#resultado = re.findall(pattern,telefone) # busca 
-resultado_tel=None
+resultado_nome = None
+resultado_tel= None
 resultado_email = None
+
+while (resultado_nome==None):
+	
+	resultado_nome = re.match(pattern_nome,nome) #procura resultado exato
+
+	if (resultado_nome != None):
+		break
+		
+	else:
+		nome = input("Digite seu nome com as letras iniciais maiúsculas: ")
+
 
 while (resultado_tel==None):
 	
@@ -45,13 +54,9 @@ while (resultado_email==None):
 		email = input("Email invalido !! digite seu email novamente: ")
 
 
-
-
-resultado_email = re.match(pattern_email,email)
-
 print("Nome: ",nome, "\nTelefone: ", resultado_tel.group(),"\nEmail:", resultado_email.group())
 
-#print(telefone)
+
 
 
 
